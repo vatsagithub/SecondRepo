43bbfd869dc34ec3702b0f273775346c7d29fa62
vatsagithub <98754307+vatsagithub@users.noreply.github.com>
2022-02-03 19:24:40 +0530

    Add files via upload

---
+++ sql server debugging.sql
@@ -0,0 +1,16 @@
+--alt + f5 for debugging, after selecting all
+
+declare @login VARCHAR(50) = ''
+declare @PkeyWebsterLoginLogId INT = 0
+
+exec usp_validateloginWrapper    
+@CorpId = 'army',      
+@EmpId = 987655184,      
+@IP = NULL,      
+@App  = 'EPayIntegrationControlPanelAuth',      
+@PkeyWebsterLoginLogId = @PkeyWebsterLoginLogId OUTPUT,  
+@PayrollNumber = ''  ,   
+@RefUrl  = '',  
+@Login= @login OUTPUT
+
+select @PkeyWebsterLoginLogId, @Login
